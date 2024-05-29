8020189be338bcd72796c09daf8d1b519f45e343
jossduff <jduff360@gmail.com>
2024-05-28 23:11:04 -0400

    custom serialize/deserialize

--- src/query.rs
+++ src/query.rs
@@ -1,5 +1,10 @@
+use core::fmt;
+
 use anyhow::{Context, Result};
-use serde::{Deserialize, Serialize};
+use serde::{
+    de::{self, Visitor},
+    Deserialize, Deserializer, Serialize, Serializer,
+};
 
 #[napi(object)]
 #[derive(Default, Serialize, Deserialize, Clone, Debug)]
@@ -18,13 +23,13 @@ pub struct ReceiptSelection {
     pub recipient: Option<Vec<String>>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub contract_id: Option<Vec<String>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none", with = "vec_u64_as_string")]
     pub ra: Option<Vec<String>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none", with = "vec_u64_as_string")]
     pub rb: Option<Vec<String>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none", with = "vec_u64_as_string")]
     pub rc: Option<Vec<String>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none", with = "vec_u64_as_string")]
     pub rd: Option<Vec<String>>,
 }
 
@@ -130,5 +135,110 @@ impl TryFrom<hyperfuel_net_types::Query> for Query {
     fn try_from(hyperfuel_query: hyperfuel_net_types::Query) -> Result<Self> {
         let json = serde_json::to_vec(&hyperfuel_query).context("serialize query to json")?;
         serde_json::from_slice(&json).context("parse json")
+    }
+}
+
+mod vec_u64_as_string {
+    use super::*;
+
+    pub fn serialize<S>(opt: &Option<Vec<String>>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        if let Some(vec) = opt {
+            let u64_vec: Vec<u64> = vec.iter().map(|s| s.parse().unwrap_or(0)).collect();
+            serializer.serialize_some(&u64_vec)
+        } else {
+            serializer.serialize_none()
+        }
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Option<Vec<String>>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct VecU64Visitor;
+
+        impl<'de> Visitor<'de> for VecU64Visitor {
+            type Value = Option<Vec<u64>>;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("an optional vector of u64")
+            }
+
+            fn visit_some<D>(self, deserializer: D) -> Result<Self::Value, D::Error>
+            where
+                D: Deserializer<'de>,
+            {
+                let vec: Vec<u64> = Deserialize::deserialize(deserializer)?;
+                Ok(Some(vec))
+            }
+
+            fn visit_none<E>(self) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+            {
+                Ok(None)
+            }
+        }
+
+        let opt = deserializer.deserialize_option(VecU64Visitor)?;
+        Ok(opt.map(|vec| vec.iter().map(|&num| num.to_string()).collect()))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_serialization_of_receipt_selection() {
+        let selection: ReceiptSelection = ReceiptSelection {
+            root_contract_id: None,
+            to_address: None,
+            asset_id: None,
+            receipt_type: None,
+            sender: None,
+            recipient: None,
+            contract_id: None,
+            ra: Some(vec!["1234".into()]),
+            rb: Some(vec!["21".into(), "9999999999".into()]),
+            rc: Some(vec![format!("{}", u64::MAX)]),
+            rd: None,
+        };
+
+        let se = serde_json::to_string(&selection).unwrap();
+
+        let de: ReceiptSelection = serde_json::from_str(&se).unwrap();
+
+        assert_eq!(de.ra, Some(vec!["1234".into()]));
+        assert_eq!(de.rb, Some(vec!["21".into(), "9999999999".into()]));
+        assert_eq!(de.rc, Some(vec![format!("{}", u64::MAX)]));
+        assert_eq!(de.rd, None);
+    }
+
+    #[test]
+    fn test_serialization_into_net_types() {
+        let selection: ReceiptSelection = ReceiptSelection {
+            root_contract_id: None,
+            to_address: None,
+            asset_id: None,
+            receipt_type: None,
+            sender: None,
+            recipient: None,
+            contract_id: None,
+            ra: Some(vec!["1234".into()]),
+            rb: Some(vec!["21".into(), "9999999999".into()]),
+            rc: Some(vec![format!("{}", u64::MAX)]),
+            rd: None,
+        };
+
+        let json = serde_json::to_vec(&selection).unwrap();
+        let net_type: hyperfuel_net_types::ReceiptSelection =
+            serde_json::from_slice(&json).unwrap();
+
+        assert_eq!(net_type.ra, vec![1234]);
+        assert_eq!(net_type.rb, vec![21, 9999999999]);
+        assert_eq!(net_type.rc, vec![u64::MAX]);
     }
 }
